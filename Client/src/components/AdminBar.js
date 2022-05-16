dfc5aad9700447d81636c1ed2d736db89d3bfa80
Ribenjyeo <sheepown123@gmail.com>
2022-05-16 20:05:05 +0300

    adding users from csv file

--- Client/src/components/AdminBar.js
+++ Client/src/components/AdminBar.js
@@ -4,6 +4,10 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import {useNavigate} from 'react-router-dom'
 import { useCookies } from "react-cookie";
+import { useState } from 'react';
+import axios from 'axios'
+import { useEffect } from 'react';
+import UploadIcon from '@mui/icons-material/Upload';
 
 const AdminBar = () => {
     const [cookies, setCookies, removeCookies] = useCookies(['user']);
@@ -17,6 +21,59 @@ const AdminBar = () => {
         navigate('/auth')
         window.location.reload()
     }
+
+    const [csvFile, setCsvFile] = useState()
+    const [csvArray, setCsvArray] = useState([])
+
+    const processCSV = (str, delim=";") => { //обработка текста и получение массива 
+        const headers = str.slice(0, str.indexOf('\n')).split(delim)
+
+        const rows = str.slice(str.indexOf('\n')+1, -1).split('\n')
+
+        const newArray =  rows.map ( row => { 
+            const values = row.split(delim)
+            const eachObject = headers.reduce((obj, header, i ) => {
+                obj[header] = values[i]
+                return obj
+            }, {})
+            return eachObject
+        })
+
+        for( let i = 0; i < newArray.length; i++) { //менял роль на Int
+            newArray[i].role = parseInt(newArray[i].role)
+        }
+
+        setCsvArray({'users' : newArray})
+    }
+
+    const handleFile = () => {
+        const file = csvFile
+        const reader = new FileReader()
+
+        reader.onload = function(e) { //получение текста
+            const text = e.target.result
+            processCSV(text)
+        }
+
+        reader.readAsText(file)
+
+    }
+
+    let requestCsvFile = async function () {
+
+        const response = await axios.post('/users/create', JSON.stringify(csvArray),  {headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : 'Bearer ' + cookies.AuthToken}})
+
+        window.location.reload()
+    }
+
+    useEffect(() => {
+        if(acsvFile.files[0]) {
+            handleFile()
+        }
+    })
+
     return (
         <>
         <div className='AdminBar'>
@@ -30,10 +87,17 @@ const AdminBar = () => {
                         <ul>
                             <li><SettingsIcon className='adminbarIcons'/>
                                 <ul className='dropdown'>
-                                    <li>Импортировать пользователей</li>
-                                    <li>Экспортировать пользователей</li>
-                                    <li>Импортировать задание</li>
-                                    <li>Экспортировать задание</li>
+                                    <li><p onClick={(e) => {requestCsvFile()}}>Импортировать пользователей</p>
+                                        <input className='CsvFile'
+                                        type="file"
+                                        accept=".csv"
+                                        id="acsvFile"
+                                        onChange={(e) => {setCsvFile(e.target.files[0])}}
+                                        />
+                                    </li>
+                                    {/* <li>Экспортировать пользователей</li> */}
+                                    {/* <li>Импортировать задание</li>
+                                    <li>Экспортировать задание</li> */}
                                 </ul>
                             </li>
                         </ul>
